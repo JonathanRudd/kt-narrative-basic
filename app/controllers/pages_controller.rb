969a8638db9553c052f565518828a441a74ec8d9
Jonathan Rudd <jonathanrudd3@gmail.com>
2023-03-31 16:06:58 +0900

    created horribly broken index on homepage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,14 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  # skip_before_action :authenticate_user!, only: [ :home ]
+
+  before_action :set_killteam, only: %i[ show edit update destroy ]
+
+  # GET /killteams or /killteams.json
+
+
+  def index
+    @killteams = Killteam.all
+  end
 
   def home
   end
